72b65b78c287219259dca9dbb3484bac97473419
Rodion Basmanov <st135699@student.spbu.ru>
2025-06-01 18:54:35 +0300

    File added skip_list.cpp , which is left almost empty for future non-template functions or explicit template instantiations. The SkipList implementation is in the header file.

---
+++ src/skip_list.cpp
@@ -0,0 +1,9 @@
+#include "../include/skip_list.h"
+
+// This file is intentionally left almost empty because the SkipList is a template class
+// and all the implementation is in the header file.
+// This file is provided for potential future non-template functions or explicit template instantiations.
+
+namespace stl_containers {
+    // Any non-template utility functions would go here
+} 
